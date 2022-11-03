40d667db13627449d37134a97506277a9276f76f
joun <joao.gutstein@gft.com>
2022-11-03 11:06:08 -0300

    repositories implementados

---
+++ src/main/java/com/joao/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.joao.bookstore.repositories;
+
+import com.joao.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/com/joao/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.joao.bookstore.repositories;
+
+import com.joao.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
