47c80820780efb26c17834d990db797e90ca2071
joun <joao.gutstein@gft.com>
2022-11-03 10:58:47 -0300

    finalizando as clases de dominio

--- src/main/java/com/joao/bookstore/domain/Livro.java
+++ src/main/java/com/joao/bookstore/domain/Livro.java
@@ -1,14 +1,20 @@
 package com.joao.bookstore.domain;
 
-public class Livro {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Livro implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
-
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
-
-
 
     public Livro() {
     }
